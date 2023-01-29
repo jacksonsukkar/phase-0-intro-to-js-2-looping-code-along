bdf22056fa22c00c7298dd7cca7790c358b273ca
Jackson <jacksonsukkar91@gmail.com>
2023-01-29 18:07:42 -0500

    submitting lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const thankYouCards = [];
+const myEvent = "party";
+const names = ["Jamal", "Benjamin", "Arturo"];
+
+function writeCards(names, eventName) {
+  for (let i = 0; i < names.length; i++) {
+    thankYouCards.push('Thank you, ' + names[i] + ' , for coming to my ' + eventName + '!');
+    
+  }
+  
+  return thankYouCards;
+}
+writeCards(names, myEvent);
+console.log(thankYouCards)
+
+function countDown() {
+  let countDown = 10;
+  while (countdown >= 0) {
+    console.log(countdown--);
+  }
+}
